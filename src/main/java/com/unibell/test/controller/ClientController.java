12b06860b1ecbad85014566d7763f8e0c39e59ad
margo <margo171190@mail.ru>
2024-03-06 05:15:32 +0400

    added javadocs

--- src/main/java/com/unibell/test/controller/ClientController.java
+++ src/main/java/com/unibell/test/controller/ClientController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller for handling client requests
+ */
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
@@ -17,16 +20,30 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    /**
+     * Adds a client
+     * @param clientDetails is a DTO that contains name
+     * @return ResponseEntity<ClientDto> consisting of Client DTO(id and name) and Http status
+     */
     @PostMapping
     public ResponseEntity<ClientDto> addClient(@RequestBody CreateOrFindClientDto clientDetails){
         return new ResponseEntity<>(clientService.addClient(clientDetails), HttpStatus.CREATED);
     }
 
+    /**
+     * Gets all clients
+     * @return ResponseEntity<ClientsDto> consisting of Clients DTO(a list of clients and the size of the list) and Http status
+     */
     @GetMapping
     public ResponseEntity<ClientsDto> getAllClients(){
         return new ResponseEntity<>(clientService.getAllClients(), HttpStatus.OK);
     }
 
+    /**
+     * Gets info about a client
+     * @param id of a client
+     * @return ResponseEntity<CreateOrFindClientDto> consisting of CreateOrFindClientDto(name) and Http status
+     */
     @GetMapping("/{id}")
     public ResponseEntity<CreateOrFindClientDto> getClientInfo(@PathVariable long id) {
         return new ResponseEntity<>(clientService.getClientInfo(id), HttpStatus.OK);
--- src/main/java/com/unibell/test/controller/ContactController.java
+++ src/main/java/com/unibell/test/controller/ContactController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller for handling contact requests
+ */
 @RestController
 @RequestMapping("/clients")
 public class ContactController {
@@ -17,17 +20,34 @@ public class ContactController {
         this.contactService = contactService;
     }
 
+    /**
+     * Adds a new contact for a client
+     * @param id of a client
+     * @param contactDetails is DTO containing contact type and contact value
+     * @return ResponseEntity<ContactDto> consisting of Contact DTO(id, contact type, contact value, client id) and Http status
+     */
     @PostMapping("/{id}/contacts")
     public ResponseEntity<ContactDto> addContactToClient(@PathVariable long id,
                                                          @RequestBody CreateContactDto contactDetails){
         return new ResponseEntity<>(contactService.addContactToClient(id, contactDetails), HttpStatus.CREATED);
     }
 
+    /**
+     * Gets a list of contacts of a certain client
+     * @param id of a client
+     * @return ResponseEntity<ContactsDto> consisting of Contacts DTO(a list of contacts and the size of the list) and Http status
+     */
     @GetMapping("/{id}/contacts")
     ResponseEntity<ContactsDto> getContactsByClientId(@PathVariable long id){
         return new ResponseEntity<>(contactService.getContactsByClientId(id), HttpStatus.OK);
     }
 
+    /**
+     * Gets a list of contacts of a certain type and of a certain client
+     * @param id of a client
+     * @param contactType is a contact type
+     * @return ResponseEntity<ContactsDto> consisting of Contacts DTO(a list of contacts and the size of the list) and Http status
+     */
     @GetMapping("/{id}/contacts/{contactType}")
     ResponseEntity<ContactsDto> getContactsByClientId(@PathVariable long id,
                                                       @PathVariable String contactType){
